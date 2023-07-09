e1b7e06d01248133e6cd69affbdf272c181cb23f
Nirob Barman <niroborin111@gmail.com>
2023-07-09 08:08:17 +0600

    Module 17

---
+++ Module 17/postorder_traversal.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int _value)
+    {
+        this->value = _value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void postOrder(Node *Root)
+{
+    if (Root == NULL)
+        return;
+
+    postOrder(Root->left);
+    postOrder(Root->right);
+    cout << Root->value << " ";
+}
+
+int main()
+{
+    Node *Root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    Root->left = a;
+    Root->right = b;
+
+    a->left = c;
+    a->right = h;
+
+    c->right = e;
+
+    b->right = d;
+
+    d->left = f;
+    d->right = g;
+
+    h->right = i;
+
+    postOrder(Root);
+
+    return 0;
+}
